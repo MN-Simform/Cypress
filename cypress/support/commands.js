273d3817e5b8b7b8249dae00be7816b37d77ab03
MN-Simform <nimit@simformsolutions.com>
2024-06-03 14:50:22 +0530

    Handled Uncaught Exception

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -26,3 +26,21 @@
 
 import 'cypress-file-upload';
 import '@4tw/cypress-drag-drop'
+import 'cypress-iframe';
+
+Cypress.on('uncaught:exception', (err, runnable) => {
+    // Ignore the specific error regarding 'msie' property being undefined
+    if (err.message.includes("Cannot read properties of undefined (reading 'msie')")) {
+        return false;
+    }
+    // Ignore the script error from a cross-origin script
+    if (err.message.includes("Script error.")) {
+        return false;
+    }
+    // Ignore the cross-origin frame error
+    if (err.message.includes('Blocked a frame with origin')) {
+        return false;
+    }
+    // Allow other errors to fail the test
+    return true;
+});
