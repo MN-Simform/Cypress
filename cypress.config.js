58b8b678b808cfabf2c14ed9efa78067755b9cbf
MN-Simform <nimit@simformsolutions.com>
2024-06-04 13:12:48 +0530

    Add Cypress Cloud Project ID

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "tvstxc",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
